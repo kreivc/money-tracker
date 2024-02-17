98cdf05c76168f5a31fb72d4c419a2747a1c7fcd
Kreivc <phangr28@gmail.com.com>
2024-02-17 16:07:43 +0700

    fix auth

--- packages/app/features/signup/email-verification/screen.tsx
+++ packages/app/features/signup/email-verification/screen.tsx
@@ -6,47 +6,52 @@ import { useRouter } from "solito/router";
 import { trpc } from "app/utils/trpc";
 
 export function EmailVerificationScreen() {
-  const { push } = useRouter();
-  const [verificationCode, setVerificationCode] = useState("");
-  const createUserMutation = trpc.user.create.useMutation();
+	const { push } = useRouter();
+	const [verificationCode, setVerificationCode] = useState("");
+	const createUserMutation = trpc.user.create.useMutation();
 
-  const { signUp, setSession } = useSignUp();
-  if (!signUp) return null;
+	const { signUp, setSession } = useSignUp();
+	if (!signUp) return null;
 
-  const handleEmailVerificationOnPress = async () => {
-    /* verify the email */
-    await signUp.attemptEmailAddressVerification({ code: verificationCode });
+	const handleEmailVerificationOnPress = async () => {
+		/* verify the email */
+		try {
+			await signUp.attemptEmailAddressVerification({ code: verificationCode });
+		} catch (err) {
+			console.log(err);
+			return alert("Invalid verification code");
+		}
 
-    if (signUp.status === "complete") {
-      const { createdSessionId } = signUp;
-      if (createdSessionId) {
-        await setSession(createdSessionId);
-      }
-      /* add user id and email into our database */
-      createUserMutation.mutate({
-        id: signUp.createdUserId!,
-        email: signUp.emailAddress!,
-      });
-      push("/");
-    } else alert("Invalid verification code");
-  };
-  return (
-    <YStack f={1} jc="center" ai="center" space>
-      <Input
-        placeholder="Verification code"
-        onChangeText={(text) => {
-          setVerificationCode(text);
-        }}
-      />
+		if (signUp.status === "complete") {
+			const { createdSessionId } = signUp;
+			if (createdSessionId) {
+				await setSession(createdSessionId);
+			}
+			/* add user id and email into our database */
+			createUserMutation.mutate({
+				id: signUp.createdUserId!,
+				email: signUp.emailAddress!,
+			});
+			push("/");
+		} else alert("Invalid verification code");
+	};
+	return (
+		<YStack f={1} jc="center" ai="center" space>
+			<Input
+				placeholder="Verification code"
+				onChangeText={(text) => {
+					setVerificationCode(text);
+				}}
+			/>
 
-      {/* button for submitting */}
-      <Button
-        onPress={() => {
-          handleEmailVerificationOnPress();
-        }}
-      >
-        Submit
-      </Button>
-    </YStack>
-  );
+			{/* button for submitting */}
+			<Button
+				onPress={() => {
+					handleEmailVerificationOnPress();
+				}}
+			>
+				Submit
+			</Button>
+		</YStack>
+	);
 }
--- packages/app/utils/auth.tsx
+++ packages/app/utils/auth.tsx
@@ -1,115 +1,115 @@
 import * as AuthSession from "expo-auth-session";
 import {
-  OAuthStrategy,
-  SetSession,
-  SignUpResource,
-  SignInResource,
+	OAuthStrategy,
+	SetSession,
+	SignUpResource,
+	SignInResource,
 } from "@clerk/types";
 
 export const handleOAuthSignUp = async (
-  strategy: OAuthStrategy,
-  setSession: SetSession,
-  signUp: SignUpResource
+	strategy: OAuthStrategy,
+	setSession: SetSession,
+	signUp: SignUpResource
 ) => {
-  try {
-    const redirectUrl = AuthSession.makeRedirectUri({
-      path: "/sso-oauth",
-      //this isn't gonna work any since we are not using the *real url that starts with expo://
-      //but that's okay, since we are going to handle the redirect ourselves
-    });
+	try {
+		const redirectUrl = AuthSession.makeRedirectUri({
+			path: "/sso-oauth",
+			//this isn't gonna work any since we are not using the *real url that starts with expo://
+			//but that's okay, since we are going to handle the redirect ourselves
+		});
 
-    await signUp.create({
-      strategy: strategy,
-      redirectUrl: redirectUrl,
-    });
+		await signUp.create({
+			strategy: strategy,
+			redirectUrl: redirectUrl,
+		});
 
-    const {
-      verifications: {
-        externalAccount: { externalVerificationRedirectURL },
-      },
-    } = signUp;
+		const {
+			verifications: {
+				externalAccount: { externalVerificationRedirectURL },
+			},
+		} = signUp;
 
-    const result = await AuthSession.startAsync({
-      authUrl: externalVerificationRedirectURL!.toString(),
-      returnUrl: redirectUrl,
-    });
+		const result = await AuthSession.startAsync({
+			authUrl: externalVerificationRedirectURL!.toString(),
+			returnUrl: redirectUrl,
+		});
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const { type, params } = result || {};
-    if (type !== "success") {
-      throw "Something went wrong during the OAuth flow. Try again.";
-    }
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		const { type, params } = result || {};
+		if (type !== "success") {
+			throw "Something went wrong during the OAuth flow. Try again.";
+		}
 
-    // Get the rotatingTokenNonce from the redirect URL parameters
-    const { rotating_token_nonce: rotatingTokenNonce } = params;
+		// Get the rotatingTokenNonce from the redirect URL parameters
+		const { rotating_token_nonce: rotatingTokenNonce } = params;
 
-    await signUp.reload({ rotatingTokenNonce });
+		await signUp.reload({ rotatingTokenNonce });
 
-    const { createdSessionId } = signUp;
+		const { createdSessionId } = signUp;
 
-    if (!createdSessionId) {
-      throw "Something went wrong during the Sign up OAuth flow. Please ensure that all sign in requirements are met.";
-    }
+		if (!createdSessionId) {
+			throw "Something went wrong during the Sign up OAuth flow. Please ensure that all sign in requirements are met.";
+		}
 
-    await setSession(createdSessionId);
+		await setSession(createdSessionId);
 
-    return;
-  } catch (err) {
-    console.log(JSON.stringify(err, null, 2));
-    console.log("error signing up", err);
-  }
+		return;
+	} catch (err) {
+		console.log(JSON.stringify(err, null, 2));
+		console.log("error signing up", err);
+	}
 };
 export const handleOAuthSignIn = async (
-  strategy: OAuthStrategy,
-  setSession: SetSession,
-  signIn: SignInResource
+	strategy: OAuthStrategy,
+	setSession: SetSession,
+	signIn: SignInResource
 ) => {
-  try {
-    const redirectUrl = AuthSession.makeRedirectUri({
-      path: "/oauth-native-callback",
-    });
-    //option is either discord, google, or apple
-    //switch statement to handle each option
+	try {
+		const redirectUrl = AuthSession.makeRedirectUri({
+			path: "/oauth-native-callback",
+		});
+		//option is either discord, google
+		//switch statement to handle each option
 
-    await signIn.create({
-      strategy: strategy,
-      redirectUrl,
-    });
+		await signIn.create({
+			strategy: strategy,
+			redirectUrl,
+		});
 
-    const {
-      firstFactorVerification: { externalVerificationRedirectURL },
-    } = signIn;
+		const {
+			firstFactorVerification: { externalVerificationRedirectURL },
+		} = signIn;
 
-    const result = await AuthSession.startAsync({
-      authUrl: externalVerificationRedirectURL!.toString(),
-      returnUrl: redirectUrl,
-    });
+		const result = await AuthSession.startAsync({
+			authUrl: externalVerificationRedirectURL!.toString(),
+			returnUrl: redirectUrl,
+		});
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const { type, params } = result || {};
-    console.log;
-    if (type !== "success") {
-      throw "Something went wrong during the OAuth flow. Try again.";
-    }
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		const { type, params } = result || {};
+		console.log;
+		if (type !== "success") {
+			throw "Something went wrong during the OAuth flow. Try again.";
+		}
 
-    // Get the rotatingTokenNonce from the redirect URL parameters
-    const { rotating_token_nonce: rotatingTokenNonce } = params;
+		// Get the rotatingTokenNonce from the redirect URL parameters
+		const { rotating_token_nonce: rotatingTokenNonce } = params;
 
-    await signIn.reload({ rotatingTokenNonce });
+		await signIn.reload({ rotatingTokenNonce });
 
-    const { createdSessionId } = signIn;
+		const { createdSessionId } = signIn;
 
-    if (!createdSessionId) {
-      throw "Something went wrong during the Sign in OAuth flow. Please ensure that all sign in requirements are met.";
-    }
+		if (!createdSessionId) {
+			throw "Something went wrong during the Sign in OAuth flow. Please ensure that all sign in requirements are met.";
+		}
 
-    await setSession(createdSessionId);
+		await setSession(createdSessionId);
 
-    return;
-  } catch (err) {
-    console.log(JSON.stringify(err, null, 2));
-    console.log("error signing in", err);
-  }
+		return;
+	} catch (err) {
+		console.log(JSON.stringify(err, null, 2));
+		console.log("error signing in", err);
+	}
 };
